2e0c1ecbbe9b283745be79e32e50c7d00fef41c7
GY.ko <dlatl!@>
2023-09-01 18:41:32 +0900

    게시판 등록 작업 진행중 1

---
+++ freeboard_frontend/pages/boards/new/index.js
@@ -0,0 +1,21 @@
+import {
+    Title,
+    Wrapper,
+} from "../../../styles/board.write.ui";
+
+
+export default function BoardWriteUI ( ) {
+    return (
+        <Wrapper>
+            <Title></Title>
+            <div>
+                <label></label>
+                <input type="text" />
+            </div>
+            <div>
+                <label></label>
+                <input type="text" />
+            </div>
+        </Wrapper>
+    )
+}
---
+++ freeboard_frontend/styles/board.write.ui.js
@@ -0,0 +1,21 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction : column;
+    width: 1200px;
+    padding: 80px 102px 0 102px;
+    border: 1px solid #BDBDBD;
+    box-shadow: 0 4px 20px rgba(0, 0, 0, 0.2);
+
+`
+
+export const Title = styled.h1`
+    font-family: Noto Sans CJK KR;
+    font-size: 36px;
+    font-weight: 700;
+    line-height: 53px;
+    letter-spacing: 0em;
+    text-align: center;
+    color: #000;
+`
