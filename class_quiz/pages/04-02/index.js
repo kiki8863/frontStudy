f2fbeefe8ed181740b93a1faaa9ce33e92d8d375
GY.ko <kiki8863@gmail.com>
2023-09-21 13:58:33 +0900

    폴더 분리하기 완

--- class_quiz/pages/04-02/index.js
+++ class_quiz/pages/04-02/index.js
@@ -1,24 +1,59 @@
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
-import { useMutation , gql } from "@apollo/client";
+import { useMutation, gql } from "@apollo/client"
+import { useState } from "react";
 
-const client = new ApolloClient ( { 
-    uri : "http://practice.codebootcamp.co.kr/graphql",
-    cache :  new InMemoryCache( )
-} ) ;
-
-export default function graphqlApi ( ) {
-    
-
-    const onClickSubmit = ( ) => {
-        const result = await  createBoard ( ) {
-            
+const myGraphqlSet = gql `
+    mutation createBoard( $writer : String, $title : String, $contents : String ) {
+        createBoard ( writer : $writer , title: $title, contents: $contents ) {
+            _id
+            number
+            message
         }
+    }
+`
 
+export default function GraphqlMutationPage( ) {
+    
+    
+    const [ myFunction ] = useMutation( myGraphqlSet );
+
+    const [ writer , setWriter ] = useState();
+    const [ title , setTitle ] = useState();
+    const [ contents , setContents ] = useState();
+
+    const onChangeWriter = ( event ) => {
+        setWriter ( event.target.value  );
     }
 
-    return ( 
-        <ApolloProvider client={ client }>
-            <button onClick={ onClickSubmit } >GRAPHQL-API 요청하기</button>
-        </ApolloProvider>
-    )
-} 
+    const onChangeTitle= ( event ) => {
+        setTitle ( event.target.value  );
+    }
+
+    const onChangeContents = ( event ) => {
+        setContents ( event.target.value  );
+    }
+
+
+    const onClickSubmit = async () => {
+
+        const result = await myFunction( {
+                variables : {                               // variables = $ 역할을 함 
+                    writer : writer,
+                    title : title,
+                    contents : contents
+                }
+            } ); 
+            console.log( result );
+        
+    }  
+    // 한 줄일때는 괄호() 필요 없음
+    return(
+        <div>
+            작성자 : <input type="text" onChange={ onChangeWriter }/>
+            제목 : <input type="text" onChange={ onChangeTitle }/>
+            내용 : <input type="text" onChange={ onChangeContents }/>
+            <button onClick={ onClickSubmit }>Graphql-API 요청하기</button> 
+        </div>
+    ) 
+    
+    
+}
