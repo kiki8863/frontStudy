f6e1282daed9797a830c0d4da094ce7cf4c1878b
GY.ko <kiki8863@gmail.com>
2024-03-29 16:30:02 +0900

    오랜만에 공부 다시 시작

---
+++ restart2024/class/pages/section01/01-01-example/index.js
@@ -0,0 +1,13 @@
+export default function qqq() {
+
+    //자바스크립트 작성 부분
+
+    return( 
+        //html 작성 공간
+        <div>
+            <div>철수</div>
+            <button>클릭하세요!</button>
+        </div>
+    )
+
+}
